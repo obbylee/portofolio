57bee4873e558442758a8057f1fc3a660848d38a
leeobby <leeobbyw@gmail.com>
2024-03-25 15:51:53 +0700

    remove ts from sushiman

--- sushiman/src/main.jsx
+++ sushiman/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import AOS from "aos";
 import "./index.css";
 import "aos/dist/aos.css";
 
 AOS.init({ duration: 1000, offset: 100 });
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
