2a2528b3353f7ba542f69b142887f415386c9a48
leeobby <leeobbyw@gmail.com>
2024-03-25 14:26:57 +0700

    traveluiux

---
+++ traveluiux/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Travel | Hilink React + Tailwindcss + Typescript",
+  description: "Modern Travel UI/UX for Camping",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
